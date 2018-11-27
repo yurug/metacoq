4c8d896e07039ad3994345292d8679edd2a621cf
Matthieu Sozeau <mattam@mattam.org>
2018-11-27 19:51:51 +0100

    Move template_checker to mlg

--- template-coq/theories/TemplateMonad/Core.v
+++ template-coq/theories/TemplateMonad/Core.v
@@ -11,7 +11,7 @@ Set Printing Universes.
 
 (** Reduction strategy to apply, beware [cbv], [cbn] and [lazy] are _strong_. *)
 
-Inductive reductionStrategy : Set :=
+Monomorphic Inductive reductionStrategy : Set :=
   cbv | cbn | hnf | all | lazy | unfold (i : ident).
 
 Record typed_term : Type := existT_typed_term
